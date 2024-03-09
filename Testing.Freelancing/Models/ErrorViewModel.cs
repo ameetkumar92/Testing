b5d68eefc43fb3e2104e700a791ab0730a651c43
ameetkumar92 <ameetkumarit@gmail.com>
2024-03-09 13:19:31 +0530

    firstProj

---
+++ Testing.Freelancing/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Testing.Freelancing.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
